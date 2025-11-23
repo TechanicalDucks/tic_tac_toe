fae7407346ee480ecf7fae6bd2daa9de4c26ab80
htoopyaelinn56 <hplinn0@gmail.com>
2025-11-23 10:43:47 +0630

    Add player ID display and peer discovery functionality in main menu

--- godot_tic_tac_toe/main_menu_scene.gd
+++ godot_tic_tac_toe/main_menu_scene.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
-func _ready():
+var rust_node
 
-	var rust_node = RustNode.new()
+func _ready():
+	rust_node = RustNode.new()
 	add_child(rust_node)
-	
-	# Call Rust functions
+
 	rust_node.hello_world()
 	
-   
+	randomize()
+	var random_number = randi() % 20
+	var random_palyer_id = "Player %d" % random_number
+	
+	var playerIdNode = get_node("PlayerId")
+	playerIdNode.text = random_palyer_id
+	
+	rust_node.start_discovery_service(random_palyer_id)
+
+	discover_loop(random_palyer_id)
+
+func discover_loop(playerId) -> void:
+	while true:
+		var result = rust_node.discover_peers()
+		print("peers result for %s = %s" % [playerId, result])
+		await get_tree().create_timer(5.0).timeout
